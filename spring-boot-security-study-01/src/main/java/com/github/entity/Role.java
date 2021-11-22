ea4ebd667ffc1f2363f1fa71ef023d5370b5938b
YangJiaHui <819582890@qq.com>
2021-11-22 19:28:50 +0800

    first commit

---
+++ spring-boot-security-study-01/src/main/java/com/github/entity/Role.java
@@ -0,0 +1,41 @@
+package com.github.entity;
+
+/**
+  * @Description: 用户对应角色信息
+  *
+  *
+  * @date 2020/4/16 下午3:55
+  */
+public class Role {
+    private Long id;
+    private String name;
+
+
+
+    public Role() {
+    }
+
+    public Long getId() {
+
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role(Long id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+}
